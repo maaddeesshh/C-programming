bda4e62dd3eb2da116e93fcdc8fbf35806001f7b
Eugene Madegwa <maaddeesshh@gmail.com>
2025-03-25 23:02:18 -0500

    first commit

--- pointer_1.c
+++ pointer_1.c
@@ -6,7 +6,7 @@ int main() {
 
     printf("Value of num: %d\n", num);
     printf("Address of num: %p\n", &num);
-    printf("Address of num: %d\n", *&*&num);
+    printf("Value of num: %d\n", *&*&num);
     printf("Pointer ptr stores address: %p\n", ptr);
     printf("Value at the pointer location: %d\n", *ptr);  // Dereferencing
 
